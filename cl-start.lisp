e9a9d167177ba9d20d8f8d607d0f712da55283c3
Carlos Estrada <carlos@me.com>
2022-05-12 02:26:05 +0200

    new stop-server function to terminate the listener's thread

--- cl-start.lisp
+++ cl-start.lisp
@@ -15,7 +15,17 @@
 (defun bye()
   "Bye !")
 
+
+(defun stop-server ()
+	"Terminates the thread that executes the listener"
+	(sb-thread:terminate-thread (find-if
+													(lambda (th)
+														(string= (sb-thread:thread-name th)
+																		 "hunchentoot-listener-*:8080"))
+													(sb-thread:list-all-threads))))
+
 (defun main ()
+	"Starts the thread that executes the listener"
   (start (make-instance 'easy-acceptor :port 8080))
   (sb-thread:join-thread (find-if
 						  (lambda (th)
